fd6510254f1a91f2b91c6a7f9200413b3d496c01
Luis Eloy Homobono Fuentes <eloy.homobono@gmail.com>
2024-06-14 18:09:13 -0600

    add plugins for use common

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,10 @@
+return {
+  -- indent guide lines
+  "lukas-reineke/indent-blankline.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  main = "ibl",
+  opts = {
+    indent = { char = "┊" },
+  },
+}
+
